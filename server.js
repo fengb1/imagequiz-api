b46c110ca6d655e1a05d75d00ef07df535aae23e
fengb1 <fengb1@email.arizona.edu>
2020-12-16 07:21:53 -0700

    setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var cors = require("cors");
+var data = require("./data");
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  response.send("Welcome to API");
+})
+
+app.get('/quizzes', (req, res) => {
+  let metadata = data.quizzes.map(x => {
+    return {id: x.id, title: x.title, picture: x.picture};
+  });
+  response.json(data.quizzes);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
